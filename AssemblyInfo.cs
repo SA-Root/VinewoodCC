dc647b1787967c684374bbfc30edb0b8bae3322b
SA-Root <a124000@outlook.com>
2021-05-10 14:50:57 +0800

    Update Assembly

--- AssemblyInfo.cs
+++ AssemblyInfo.cs
@@ -2,5 +2,7 @@ using System.Runtime.InteropServices;
 using System.Reflection;
 
 [assembly: AssemblyProduct("Vinewood C Compiler")]
+[assembly: AssemblyTitle("Vinewood C Compiler Core")]
 [assembly: AssemblyCompany("Xavier")]
-[assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyFileVersion("1.0.2.39")]
+[assembly: AssemblyInformationalVersion("1.0.2.0")]
